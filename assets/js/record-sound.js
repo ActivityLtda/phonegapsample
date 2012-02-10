1f84288fbc105c10b9d609e450cf003269291901
Juan G. Hurtado <juan.g.hurtado@gmail.com>
2012-02-10 10:14:52 +0100

    Add capture sound sample

--- assets/js/record-sound.js
+++ assets/js/record-sound.js
@@ -12,6 +12,38 @@
 --------------------------------------------------------------------------- */
 (function() {
   $('#record-sound').bind('pagebeforeshow', function() {
+    var $page        = $(this);
+    var $recordaudio = $page.find('#record-audio');
+    var $uri         = $page.find('#uri');
 
+    /* =|Shoot video click
+    ----------------------------------------- */
+    $recordaudio.bind('click', function(e) {
+      navigator.device.capture.captureAudio(function(files) {
+        var i, len;
+        for (i = 0, len = files.length; i < len; i += 1) {
+          var path = files[i].fullPath;
+
+          $uri.text(path);
+          $page.find('source').attr('src', 'file://'+ path);
+
+          if (device.name.match(/iPhone|iPad/)) {
+            $page.find('audio').attr('src', 'file://'+ path);
+            $page.find('source').attr('type', 'audio/wav');
+          }
+        }
+
+        $page.find('#audio-wrapper').show();
+      }, recordError);
+
+      e.preventDefault();
+    });
   });
+
+  /* =|recordError(message)
+   *  - message (String): Error message
+  --------------------------------------------------------------------------- */
+  function recordError(message) {
+    navigator.notification.alert(message);
+  }
 })();
