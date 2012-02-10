c9d195924c6303a97458a72892611949a2ff54ac
Juan G. Hurtado <juan.g.hurtado@gmail.com>
2012-02-10 10:14:34 +0100

    Update video sample

--- assets/js/camera-video.js
+++ assets/js/camera-video.js
@@ -14,43 +14,30 @@
   $('#camera-video').bind('pageinit', function() {
     var $page       = $(this);
     var $shootvideo = $page.find('#shoot-video');
-    var $video      = $page.find('#video');
-    var $source     = $page.find('#source');
+    var $uri        = $page.find('#uri');
 
     /* =|Shoot video click
     ----------------------------------------- */
     $shootvideo.bind('click', function(e) {
       navigator.device.capture.captureVideo(function(files) {
-        shootSuccess($video, $source, files);
+        var i, len;
+        for (i = 0, len = files.length; i < len; i += 1) {
+          var path = files[i].fullPath;
+
+          $uri.text(path);
+          $page.find('source').attr('src', 'file://'+ path);
+
+          if (device.name.match(/iPhone|iPad/)) {
+            $page.find('video').attr('src', 'file://'+ path);
+          }
+        }
+
+        $page.find('video').show();
       }, shootError);
 
       e.preventDefault();
     });
-
-    /* =|Video click
-    ----------------------------------------- */
-    $video.bind('click', function() {
-      jQuery(this)[0].play();
-      e.preventDefault();
-    });
   });
-
-  /* =|shootSuccess($video, $source, files)
-   *  - $video (jQuery): VIDEO element to put the video on
-   *  - $source (jQuery): TEXTAREA element to put the video raw data on
-   *  - files (Array): Array of videos
-  --------------------------------------------------------------------------- */
-  function shootSuccess($video, $source, files) {
-    var i, len;
-    for (i = 0, len = files.length; i < len; i += 1) {
-      $video.attr('src', files[i].fullPath)
-        .css({
-          width : '100%'
-        }).show();
-
-      $source.text(files[i].fullPath);
-    }
-  }
 
   /* =|shootError(message)
    *  - message (String): Error message
